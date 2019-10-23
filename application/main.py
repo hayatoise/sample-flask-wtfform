48f7db658ec91a2102209fabc60e85ba4e4e4536
Hayato Ise <hayatoise.com@gmail.com>
2019-10-24 02:33:43 +0900

    fix: main の index と profile アクションの返り値を Response に変更

--- application/main.py
+++ application/main.py
@@ -1,14 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, make_response, Response
 from flask_login import current_user
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
-def index():
-    return render_template('index.html')
+def index() -> Response:
+    return make_response(render_template('index.html'))
 
 
 @main.route('/profile')
-def profile():
-    return render_template('profile.html', name=current_user.name)
+def profile() -> Response:
+    return make_response(render_template('profile.html', name=current_user.name))
