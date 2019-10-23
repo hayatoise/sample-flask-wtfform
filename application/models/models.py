fa5cbfa218a903fad097cb080cb1fc9c8c7676c2
Hayato Ise <hayatoise.com@gmail.com>
2019-10-24 03:18:36 +0900

    fix: users テーブルの email カラムの文字数を 256 から254 に変更

--- application/models/models.py
+++ application/models/models.py
@@ -16,7 +16,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(15, collation='utf8mb4_general_ci'), nullable=False)
     display_name = db.Column(db.String(50, collation='utf8mb4_general_ci'), nullable=False)
-    email = db.Column(db.String(256, collation='utf8mb4_general_ci'), nullable=False)
+    email = db.Column(db.String(254, collation='utf8mb4_general_ci'), nullable=False)
     password = db.Column(db.String(1024, collation='utf8mb4_general_ci'), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
