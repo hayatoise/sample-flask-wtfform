ef3f334a115037db00298cc01907b0bf2f527b51
Hayato Ise <hayatoise.com@gmail.com>
2019-10-23 23:14:25 +0900

    feat: 通常の HTML のフォームを WTforms に変更

--- application/auth.py
+++ application/auth.py
@@ -6,6 +6,7 @@ from flask_login import login_user, login_required, logout_user
 
 from application.models import User
 from application.database import db
+from application.forms.signup import SignupForm
 
 auth = Blueprint('auth', __name__)
 
@@ -53,75 +54,33 @@ def login_post():
     return redirect(url_for('main.profile'))
 
 
-@auth.route('/signup', methods=['GET'])
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup() -> Text:
-    return render_template('signup.html')
+    signup_form = SignupForm()
 
+    if request.method == 'POST':
+        if signup_form.validate_on_submit():
+            form = dict(request.form)
+            email: str = form.get('email')
+            name: str = form.get('name')
+            display_name: str = form.get('display_name')
+            password: str = form.get('password')
+            hashed_password: str = generate_password_hash(password, method='sha256')
+            new_user = User(name=name, display_name=display_name, email=email, password=hashed_password)
 
-@auth.route('/signup', methods=['POST'])
-def signup_post() -> Text:
-    name = request.form.get('name')
-    display_name = request.form.get('display_name')
-    email = request.form.get('email')
-    password = request.form.get('password')
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+            except Exception as e:
+                print(e)
+                db.session.rollback()
+                raise
+            finally:
+                db.session.close()
 
-    # フォームの空欄を確認
-    if name == '' or display_name == '' or email == '' or password == '':
-        flash('ユーザ名、表示名、メールアドレスまたはパスワードが空欄です')
-        return redirect(url_for('auth.signup'))
+            return redirect(url_for('auth.login'))
 
-    # メールアドレスの重複を確認
-    user_by_email = User.query.filter_by(email=email).first()
-    if user_by_email:
-        flash('このメールアドレスは既に使われています')
-        return redirect(url_for('auth.signup'))
-
-    # ユーザ名の重複を確認
-    user_by_name = User.query.filter_by(name=name).first()
-    if user_by_name:
-        flash('このユーザ名は既に使われています')
-        return redirect(url_for('auth.signup'))
-
-    # ユーザ名は 2 ~ 15 文字以内
-    if not 2 <= len(name) <= 15:
-        flash('ユーザ名は 2 ~ 15 文字以内にして下さい')
-        return redirect(url_for('auth.signup'))
-
-    # ユーザ名は英数字のみ
-    if name.isalnum() is True:
-        name = request.form.get('name').lower()
-    else:
-        flash('ユーザ名は英数字のみにして下さい')
-
-    # 表示名は 50 文字以内
-    if not 1 <= len(display_name) <= 50:
-        flash('表示名は 50 文字以内にして下さい')
-        return redirect(url_for('auth.signup'))
-
-    # メールアドレスは 6 ~ 254 文字以内
-    if not 6 <= len(email) <= 254:
-        flash('メールアドレスは 6 ~ 254 文字以内にして下さい')
-        return redirect(url_for('auth.signup'))
-
-    # パスワードの長さは 12 文字以上
-    if not 12 <= len(password):
-        flash('パスワードは 12 文字以上にして下さい')
-        return redirect(url_for('auth.signup'))
-
-    hashed_password = generate_password_hash(password, method='sha256')
-
-    new_user = User(name=name, display_name=display_name, email=email, password=hashed_password)
-
-    try:
-        db.session.add(new_user)
-        db.session.commit()
-    except Exception:
-        db.session.rollback()
-        raise
-    finally:
-        db.session.close()
-
-    return redirect(url_for('auth.login'))
+    return render_template('signup.html', form=signup_form)
 
 
 @auth.route('/logout')
--- application/forms/signup.py
+++ application/forms/signup.py
@@ -1,39 +1,42 @@
-from wtforms import Form, StringField, PasswordField
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Length, DataRequired, Email, Regexp, ValidationError
 from wtforms.fields.html5 import EmailField
 
 from application.models import User
 
 
-class SignupForm(Form):
-    email = EmailField('メールアドレス', [
+class SignupForm(FlaskForm):
+    email = EmailField('メールアドレス', validators=[
         DataRequired(message='メールアドレスは必須です'),
         Email(message='メールアドレスが不正です'),
-        Length(min=6, max=254, message='メールアドレスの長さが不正です')
-    ])
+        Length(min=6, max=254, message='メールアドレスの長さが不正です'),
+    ], render_kw={'type': 'email', 'placeholder': 'example@gmail.com'})
 
-    name = StringField('ユーザ名', [
+    name = StringField('ユーザ名', validators=[
         DataRequired(message='ユーザ名は必須です'),
         Regexp(regex=r'^[a-zA-Z0-9_]+$', message='ユーザ名は英数字とアンダースコアが使えます'),
         Length(min=2, max=15, message='ユーザ名は 2 ~ 15 文字以内です')
-    ])
+    ], render_kw={'type': 'text', 'placeholder': 'hayato'})
 
-    display_name = StringField('表示名', [
-        DataRequired(message='ユーザ名は必須です'),
+    display_name = StringField('表示名', validators=[
+        DataRequired(message='表示名は必須です'),
         Length(max=50, message='表示名は 50 文字以内です')
-    ])
+    ], render_kw={'type': 'text', 'placeholder': '伊勢 隼人'})
 
-    password = PasswordField('パスワード', [
+    password = PasswordField('パスワード', validators=[
         DataRequired(message='パスワードは必須です'),
-        Length(max=1024, message='パスワードは 1024 文字以内です')
-    ])
+        Length(min=12, max=1024, message='パスワードは 12 ~ 1024 文字以内です'),
+    ], render_kw={'type': 'password'})
 
-    def confirm_email_address_duplicate(self):
-        user = User.query.filter_by(email=self.email.data).first()
+    submit = SubmitField('登録する', render_kw={'class': 'button is-success'})
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('このメールアドレスは既に使われています')
 
-    def confirm_name_duplicate(self):
-        user = User.query.filter_by(name=self.name.data).first()
+    def validate_name(self, name):
+        user = User.query.filter_by(name=name.data).first()
         if user is not None:
             raise ValidationError('このユーザ名は既に使われています')
